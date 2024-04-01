f1e609798ea23ae932b93157c8f700737600521e
Vladimir-Urik <gggedrvideos@gmail.com>
2024-04-01 19:52:21 +0200

    fix errors

--- src/components/Guidelines.tsx
+++ src/components/Guidelines.tsx
@@ -1,12 +1,20 @@
 export const Guidelines = () => {
   return (
-    <div className={'absolute bottom-0 z-50 top-[70px] h-[calc(100%-70px)] w-full p-4'}>
+    <div
+      className={
+        'absolute bottom-0 top-[70px] z-50 h-[calc(100%-70px)] w-full p-4'
+      }
+    >
       <div className={'size-full rounded-md bg-black bg-opacity-60 p-4'}>
         <h2 className={'text-center text-4xl font-bold text-white'}>
           Průvodce
         </h2>
 
-        <div className={'mt-8 w-full text-lg text-white h-[calc(100%-80px)] overflow-y-scroll'}>
+        <div
+          className={
+            'mt-8 h-[calc(100%-80px)] w-full overflow-y-scroll text-lg text-white'
+          }
+        >
           <h3 className={'text-2xl font-bold text-white'}>Pravidlá</h3>
           <ul className={'mt-2 list-decimal px-8'}>
             <li>Při hře se snažte být co nejvíce upřímní.</li>
